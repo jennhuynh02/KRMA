04adf52e83df17896c9409ee636174b20df9dcc0
James <james.jiang13@gmail.com>
2020-05-11 14:38:03 -0700

    finished backend auth

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const passport = require('passport');
+
 const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
 
-
-app.use("/api/users", users);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use("/api/users", users);
+
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+app.use(passport.initialize());
+require('./config/passport')(passport); // add a config file for passport
 
 app.get("/", (req, res) => res.send("Hello World"));
 
