f7625b2bdc1612875fe4e6449bc193df119b9844
your name here <jennhuynh02@gmail.com>
2020-05-11 15:34:55 -0700

    Activated nav bar

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import MainPage from "../components/main/main_page";
-// import NavBarContainer from './nav/navbar_container';
+import NavBarContainer from '../components/navbar/navbar_container';
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 
 
 const App = () => (
   <div>
-    {/* <NavBarContainer /> */}
+    <NavBarContainer />
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
