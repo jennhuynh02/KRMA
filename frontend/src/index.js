3334a1b3c59cba2a70cfc2c6951b6e7c21899605
James <james.jiang13@gmail.com>
2020-05-11 13:17:56 -0700

    add session and route util

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
-// import { setAuthToken } from './util/session_api_util';
+import { setAuthToken } from './util/session_api_util';
 // import { logout } from './actions/session_actions';
 import axios from 'axios';
 
