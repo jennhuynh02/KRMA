7a7678f9ceab4fc8d932bde311f7e527b950d8e0
your name here <jennhuynh02@gmail.com>
2020-05-11 11:40:00 -0700

    Installed npm react etc. - main page is up

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -4,7 +4,7 @@ class MainPage extends React.Component {
   render() {
     return (
     <div>
-      <h1>Treasure Bpx</h1>
+      <h1>Treasure Box</h1>
       <footer>
         Copyright &copy; 2020 TreasureBox
       </footer>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,33 +3,33 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
-import { setAuthToken } from './util/session_api_util';
-import { logout } from './actions/session_actions';
+// import { setAuthToken } from './util/session_api_util';
+// import { logout } from './actions/session_actions';
 import axios from 'axios';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.axios = axios;
+  // window.axios = axios;
 
-  let store;
-  if (localStorage.jwtToken) {
-    setAuthToken(localStorage.jwtToken);
+  // let store;
+  // if (localStorage.jwtToken) {
+  //   setAuthToken(localStorage.jwtToken);
 
-    const decodedUser = jwt_decode(localStorage.jwtToken);
+  //   const decodedUser = jwt_decode(localStorage.jwtToken);
 
-    const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+  //   const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
 
-    store = configureStore(preloadedState);
+  //   store = configureStore(preloadedState);
 
-    const currentTime = Date.now() / 1000;
+  //   const currentTime = Date.now() / 1000;
 
-    if (decodedUser.exp < currentTime) {
-      store.dispatch(logout());
-      window.location.href = '/login';
-    }
-  } else {
-    store = configureStore({});
-  }
+  //   if (decodedUser.exp < currentTime) {
+  //     store.dispatch(logout());
+  //     window.location.href = '/login';
+  //   }
+  // } else {
+  //   store = configureStore({});
+  // }
   const root = document.getElementById('root');
 
-  ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<Root />, root);
 });
