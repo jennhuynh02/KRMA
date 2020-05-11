25470df66864a2d8346cada58435d12c112d0f45
your name here <jennhuynh02@gmail.com>
2020-05-11 09:58:50 -0700

    Added connection to Mongoose

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
 
