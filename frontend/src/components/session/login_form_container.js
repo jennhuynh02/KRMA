0067668f905b8bff4cc3697b69019396f3d79875
your name here <jennhuynh02@gmail.com>
2020-05-11 15:27:55 -0700

    Created routes for login and signup page

--- frontend/src/components/session/login_form_container.js
+++ frontend/src/components/session/login_form_container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { login } from '../actions/session_actions';
+import { login } from '../../actions/session_actions';
 import LoginForm from './login_form';
 
 const mapStateToProps = state => {
