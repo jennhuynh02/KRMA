28724c2f1b002e3b6d7b7ef8a9407d2ecbc21d2d
your name here <jennhuynh02@gmail.com>
2020-05-11 11:44:06 -0700

    Edited main page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,6 +5,7 @@ class MainPage extends React.Component {
     return (
     <div>
       <h1>Treasure Box</h1>
+      <h5>Author-Progammers: James Jiang, Michael Murry, Joshua Silva-Roland</h5>
       <footer>
         Copyright &copy; 2020 TreasureBox
       </footer>
