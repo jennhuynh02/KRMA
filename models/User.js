afc29bb87aeb90f90e916b114f2e05b55663e02d
James <james.jiang13@gmail.com>
2020-05-11 10:08:31 -0700

    add user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }, 
+  keyCount: {
+    type: Number,
+    default: 0
+  },
+  treasureId: {
+    type: Array,
+    default: []
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+})
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
