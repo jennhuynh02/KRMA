2e24a03625945cd1e2fa93e5e346f0e31e0345c1
your name here <jennhuynh02@gmail.com>
2020-05-11 20:10:49 -0700

    Styling main welcome page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,8 +3,8 @@ import { Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import MainPage from "../components/main/main_page";
 import NavBarContainer from '../components/navbar/navbar_container';
-import LoginFormContainer from "./session/login_form_container";
-import SignupFormContainer from "./session/signup_form_container";
+// import LoginFormContainer from "./session/login_form_container";
+// import SignupFormContainer from "./session/signup_form_container";
 
 
 const App = () => (
@@ -12,8 +12,8 @@ const App = () => (
     <NavBarContainer />
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
-      <AuthRoute exact path="/login" component={LoginFormContainer} />
-      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
 
       {/* <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
       <ProtectedRoute exact path="/profile" component={ProfileContainer} />
