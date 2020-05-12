3fb9f9e2e7901d78b39d0e4f8a2624b7fd04cf5e
your name here <jennhuynh02@gmail.com>
2020-05-12 14:36:35 -0700

    Removed superfluous import on app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute, AdminRoute } from '../util/route_util';
 import MainPage from "../components/main/main_page";
-import AWSCreateTreasure from './treasure/aws_create_treasure';
 import NavBarContainer from '../components/navbar/navbar_container';
 import TreasureIslandContainer from "./treasure/treasure_island_container";
-import CreateTreasureContainer from "./treasure/creature_treasure_container";
 import CollectionContainer from "./collections/collection_container";
 import AdminMainPageContainer from "./moderator/admin_main/admin_main_container"
 
