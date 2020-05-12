8d9fef352b28f48d160842be523bb874d3bb917c
your name here <jennhuynh02@gmail.com>
2020-05-12 13:50:29 -0700

    Removed old routes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import MainPage from "../components/main/main_page";
-import LoginFormContainer from "./session/login_form_container";
-import SignupFormContainer from "./session/signup_form_container";
 import AWSCreateTreasure from './treasure/aws_create_treasure';
 import NavBarContainer from '../components/navbar/navbar_container';
 import TreasureIslandContainer from "./treasure/treasure_island_container";
@@ -17,13 +15,8 @@ const App = () => (
     <NavBarContainer />
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
-      <AuthRoute exact path="/login" component={LoginFormContainer} />
-      <AuthRoute exact path="/signup" component={SignupFormContainer} />
       <Route exact path="/treasure/new" component={AWSCreateTreasure} />
 
-      {/* <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
-      <ProtectedRoute exact path="/profile" component={ProfileContainer} />
-      <ProtectedRoute exact patch="/new_tweet" component={TweetComposeContainer} /> */}
       {/* <ProtectedRoute exact path="/treasure/create" component={CreateTreasureContainer} /> */}
       <ProtectedRoute exact path="/treasureisland" component={TreasureIslandContainer} />
       <ProtectedRoute exact path="/collection" component={CollectionContainer} />
