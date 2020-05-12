ead248aa8825f82aa21da7013cc3e144d14f0835
your name here <jennhuynh02@gmail.com>
2020-05-12 01:55:31 -0700

    Created treasure island page and rerouted signup/login protected routes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,6 +3,7 @@ import { Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import MainPage from "../components/main/main_page";
 import NavBarContainer from '../components/navbar/navbar_container';
+import TreasureIslandContainer from "./treasure/treasure_island_container";
 // import LoginFormContainer from "./session/login_form_container";
 // import SignupFormContainer from "./session/signup_form_container";
 
@@ -15,8 +16,8 @@ const App = () => (
       {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
 
-      {/* <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
-      <ProtectedRoute exact path="/profile" component={ProfileContainer} />
+      <ProtectedRoute exact path="/treasureisland" component={TreasureIslandContainer} />
+      {/* <ProtectedRoute exact path="/profile" component={ProfileContainer} />
       <ProtectedRoute exact patch="/new_tweet" component={TweetComposeContainer} /> */}
     </Switch>
   </div>
---
+++ frontend/src/components/treasure/treasure_island.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TreasureIsland extends React.Component {
+  render() {
+    return (
+    <div>
+      <h1>Treasure Island Page</h1>
+    </div>
+    );
+  }
+}
+
+export default TreasureIsland;
