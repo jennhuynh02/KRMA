5ac78a5acb23aa1253d495b40863a5d66f1519d2
your name here <jennhuynh02@gmail.com>
2020-05-12 03:33:22 -0700

    Added collection button to treasure island page

--- frontend/src/components/treasure/treasure_island.js
+++ frontend/src/components/treasure/treasure_island.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class TreasureIsland extends React.Component {
-  handleCollectionLink(e) {
-    e.preventDefault();
-    location.href = "/#/collection";
-  }
+
   render() {
     return (
     <div className="treasure-island">
       <h1 className="island-header">Treasure Island Page</h1>
-      <button onClick={this.handleCollectionLink} className="session-buttons">Your Collection</button>
+      <Link to={'/collection'}>View Collection</Link>
     <div className="treasure-island-page">
     </div>
     </div>
