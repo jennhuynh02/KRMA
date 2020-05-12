24edcb31b73eea3fcfa0e54b46c6610f2f959676
your name here <jennhuynh02@gmail.com>
2020-05-12 15:36:18 -0700

    Added if state to nav bar for admin portals

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -13,7 +13,16 @@ class NavBar extends React.Component {
     }
 
     getLinks() {
-        if (this.props.loggedIn) {
+        if(this.props.loggedIn && (this.props.user.email === 'admin@treasurebox.com')) {
+            return (
+                <div className="page-navbar">                    
+                    <button className="session-buttons" onClick={this.logoutUser}>Log Out</button>
+                    {/* <button className="session-buttons" onClick={this.logoutUser}>Log Out</button>
+                    <button className="session-buttons" onClick={this.logoutUser}>Log Out</button>
+                    <button className="session-buttons" onClick={this.logoutUser}>Log Out</button> */}
+                </div>
+            );
+        } else if (this.props.loggedIn) {
             return (
                 <div className="page-navbar">                    
                     <button className="session-buttons" onClick={this.logoutUser}>Log Out</button>
