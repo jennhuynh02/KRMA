f8c09c833b462e9329a5243c1704a346ef826f17
your name here <jennhuynh02@gmail.com>
2020-05-13 13:08:17 -0700

    Added collection items container and component

---
+++ frontend/src/components/collections/item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TreasureItem extends React.Component {
+  render() {
+    return (
+    <div className="collection-item">
+
+    </div>
+    );
+  }
+}
+
+export default TreasureItem;
---
+++ frontend/src/components/collections/item_container.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+import TreasureItem from './item'
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TreasureItem);
