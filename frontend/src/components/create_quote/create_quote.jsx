cd21bd5ad2cb22802f8fa9c365a05719dfa077c0
jsilvaroland <joshuasilvaroland@gmail.com>
2020-05-13 15:30:17 -0700

    Add quote upload frontend logic

--- frontend/src/components/create_quote/create_quote.jsx
+++ frontend/src/components/create_quote/create_quote.jsx
@@ -4,7 +4,7 @@ class CreateQuote extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedFile: null,
+      quote: "",
       error: "",
     };
 
@@ -12,26 +12,21 @@ class CreateQuote extends React.Component {
     this.handleUpload = this.handleUpload.bind(this);
   }
 
-  handleFile(e) {
+  handleQuote(e) {
     this.setState({
-      selectedFile: e.currentTarget.files[0],
+      quote: e.currentTarget.value,
     });
   }
 
-  handleSubmit(e) {
+  handleUpload(e) {
     e.preventDefault();
     let data = new FormData();
-    if (this.state.selectedFile) {
-      data.append(
-        "profileImage",
-        this.state.selectedFile,
-        this.state.selectedFile.name
-      );
-      data.append("ownerId", this.props.currentUser.id);
-      this.props.createTreasure(data);
-      this.setState({
-        selectedFile: "",
-      });
+    if (this.state.quote !== "") {
+      data.append("quoteText", this.state.quote);
+			data.append("ownerId", this.props.currentUser.id);
+			this.props.createTreasure(data); // failing POST request currently
+			this.setState({ quote: "" });
+			// maybe render a message to user saying "Upload successful!" as bonus feature
     } else {
       this.setState({
         error: "Please enter a quote",
@@ -40,21 +35,29 @@ class CreateQuote extends React.Component {
   }
 
   render() {
-    return (
+		return (
       <div className="create-quote-wrapper">
         <button className="close-modal" onClick={this.props.closeModal}>
           X
         </button>
+        <button onClick={() => this.props.openModal("photo")}>Photo</button>
+        <button onClick={() => this.props.openModal("quote")}>Quote</button>
         <div className="quote-header">
-          <h3 style={{ color: "#555", marginLeft: "12px" }}></h3>
-            Add a Photo to the Treasure Box
+          <h3 style={{ color: "#555", marginLeft: "12px" }}>
+            Add a Quote to the Treasure Box
+          </h3>
         </div>
         <div className="quote-body">
-          <input type="textarea" onChange={this.handleQuote} />
+          <input
+            type="textarea"
+            value={this.state.quote}
+            onChange={this.handleQuote}
+          />
           <div className="mt-5">
             {this.state.error}
-            <button className="submit" onClick={this.handleSubmit}>
-              Submit
+            <br />
+            <button className="submit" onClick={this.handleUpload}>
+              Add Quote
             </button>
             <br />
             <button onClick={() => this.props.openModal("retrieve")}>
