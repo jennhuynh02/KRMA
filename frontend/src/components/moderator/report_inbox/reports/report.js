0e29b79e3fa517d8f4e0698caa32ed6b201464f3
your name here <jennhuynh02@gmail.com>
2020-05-15 12:05:12 -0700

    Rendered reason for reported flagged content

--- frontend/src/components/moderator/report_inbox/reports/report.js
+++ frontend/src/components/moderator/report_inbox/reports/report.js
@@ -8,12 +8,15 @@ class Report extends React.Component {
     id = user._id;
     let imgUrl 
     imgUrl = treasure.url
+    let reportMsg;
+    reportMsg = treasure.reportMessage
     return (
     <div className="the-collection">
     <div className="reports-box">
       <h1>{user.firstName} {user.lastName}</h1>
       <h1>{user.email}</h1>
         <img className="content-image-rp" src={imgUrl} />
+        <p>Report Message:  "{reportMsg}"</p>
       <button className="reports-button" onClick={() => deleteSelectedUser(id)}>Delete User</button>
     </div>
     </div>
