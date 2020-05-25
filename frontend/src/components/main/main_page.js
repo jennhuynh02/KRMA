adf4cb90430f22684e4b14ce0b0aa22c70ba91a7
your name here <jennhuynh02@gmail.com>
2020-05-25 01:59:13 -0700

    Adding fontawesome links to author prommaers' profiles

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,28 +1,70 @@
 import React from 'react';
-import LoginFormContainer from '../session/login_form_container'
-import SignupFormContainer from "../session/signup_form_container";
+import LoginFormContainer from '../session/login_form_container';
+import SignupFormContainer from '../session/signup_form_container';
 
 class MainPage extends React.Component {
   render() {
     return (
-    <div className="main-page-content">
+      <div className="main-page-content">
         <div className="login-signup-container">
           <h1 className="session-header">Login to unlock your treasures:</h1>
-            <div className="forms">
-              <SignupFormContainer />
-              <LoginFormContainer />
-            </div>
+          <div className="forms">
+            <SignupFormContainer />
+            <LoginFormContainer />
+          </div>
         </div>
 
-      <footer className="mainpage-footer">
-      <p>Author-Programmers' LinkedIn: </p>
-        <a href="/"><input className="linked-in-links" type="button" value="James Jiang"/></a>
-        <a href="https://www.linkedin.com/in/jenniferanhhuynh/"><input className="linked-in-links" type="button" value="Jennifer Huynh"/></a>
-        <a href="https://www.linkedin.com/in/joshua-silva-roland-0b9b951a9/"><input className="linked-in-links" type="button" value="Joshua Silva-Roland"/></a>
-        <a href="https://www.linkedin.com/in/michael-murry-b3746a1a6/"><input className="linked-in-links" type="button" value=" Michael Murry"/></a>
-      <p>Copyright &copy; 2020 TreasureBox</p>
-      </footer>
-    </div>
+        <footer className="mainpage-footer">
+
+
+          <div className="authors">
+
+            <span className="programmer">
+            <a href="https://www.linkedin.com/in/jamesjiang13/">
+              <i className="fa fa-linkedin-square" style={{ color: '#0077b5', fontSize: '45px' }} />
+            </a>
+            <a href="https://github.com/jamesjiang13">
+              <i className="fa fa-github" style={{ color: 'black', fontSize: '45px' }} />
+            </a>
+            James Jiang
+            </span>
+
+            <span className="programmer">
+            <a href="https://www.linkedin.com/in/jenniferanhhuynh/">
+              <i className="fa fa-linkedin-square" style={{ color: '#0077b5', fontSize: '45px' }} />
+            </a>
+            <a href="https://github.com/jennhuynh02">
+              <i className="fa fa-github" style={{ color: 'black', fontSize: '45px' }} />
+            </a>
+            Jennifer Huynh
+            </span>
+
+            <span className="programmer">
+            <a href="https://www.linkedin.com/in/michael-murry-b3746a1a6/">
+              <i className="fa fa-linkedin-square" style={{ color: '#0077b5', fontSize: '45px' }} />
+            </a>
+            <a href="https://github.com/MichaelMurry49">
+              <i className="fa fa-github" style={{ color: 'black', fontSize: '45px' }} />
+            </a>
+            Michael Murry
+            </span>
+
+            <span className="programmer">
+            <a href="https://www.linkedin.com/in/joshua-silva-roland-0b9b951a9/">
+              <i className="fa fa-linkedin-square" style={{ color: '#0077b5', fontSize: '45px' }} />
+            </a>
+            <a href="https://github.com/jsilvaroland">
+              <i className="fa fa-github" style={{ color: 'black', fontSize: '45px' }} />
+            </a>
+            Joshua Silva
+            </span>
+
+
+
+          </div>
+          <p>Copyright &copy; 2020 TreasureBox</p>
+        </footer>
+      </div>
     );
   }
 }
