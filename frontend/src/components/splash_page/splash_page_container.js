5c2e75c323fcc83681cbca67e485a6b7760651d5
James <james.jiang13@gmail.com>
2020-06-30 01:26:03 -0700

    Added instructions to splash page

--- frontend/src/components/splash_page/splash_page_container.js
+++ frontend/src/components/splash_page/splash_page_container.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import { login } from '../../actions/session_actions';
+import { openModal } from '../../actions/modal_actions';
 import SplashPage from './splash_page';
 
 const mapDispatchToProps = (dispatch) => ({
   login: (user) => dispatch(login(user)),
+  openModal: (modal) => dispatch(openModal(modal)),
 });
 
 export default connect(null, mapDispatchToProps)(SplashPage);
